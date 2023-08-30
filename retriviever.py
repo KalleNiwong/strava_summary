7ef697f0d3d280ce7589c0cac6b2f6790def1f8f
Karl Niwong <niwong98@gmail.com>
2023-08-30 17:35:27 +0200

    data collected

---
+++ retriviever.py
@@ -0,0 +1,24 @@
+# ONLY RUN THIS ONE TIME!
+import requests
+import json
+# Make Strava auth API call with your 
+# client_code, client_secret and code
+response = requests.post(
+                    url = 'https://www.strava.com/oauth/token',
+                    data = {
+                            'client_id': 112958,
+                            'client_secret': 'a27cd0a454772f54bbd842a8dad00c9075666e89',
+                            'code': '47a4f5f508f5541ae726f163d35565d96bf45fbc',
+                            'grant_type': 'authorization_code'
+                            }
+                )
+#Save json response as a variable
+strava_tokens = response.json()
+# Save tokens to file
+with open('strava_tokens.json', 'w') as outfile:
+    json.dump(strava_tokens, outfile)
+# Open JSON file and print the file contents 
+# to check it's worked properly
+with open('strava_tokens.json') as check:
+  data = json.load(check)
+print(data)
